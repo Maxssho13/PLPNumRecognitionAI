ed7198f8be74fe382ac13b0b6115da54f3418411
Maxssho13 <33611513+Maxssho13@users.noreply.github.com>
2020-01-15 20:11:59 -0500

    T2 Goal done
    Finished animation and feedforward. Considering adding variable amount of hidden layers.

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 let net = new Network(7, 8, 2);
-testRan = new Matrix(100, 1);
-testRan.randomize(0, 100);
-
 
 
 function setup() {
-  createCanvas(1000, 1000);
-  frameRate(1);
+  createCanvas(1000, 4000);
+  frameRate(1)
+
   net.inputLayer.randomize(0, 1);
   net.feedForward();
   console.table(net.hiddenLayer.matrix);
@@ -14,6 +12,8 @@ function setup() {
 }
 
 function draw() {
+  net.weightsHidIn.randomize(-1, 1);
+  net.weightsOutHid.randomize(-1, 1);
   background(255);
   for (let i = 0; i < net.inputLayer.matrix.length; i++) {
     ellipse(100, 50 + i * 60, 40);
@@ -29,15 +29,14 @@ function draw() {
   for (let i = 0; i < net.inputLayer.matrix.length; i++) {
     a = -60 + i * -60;
     yOffset = -10;
-    //draw hidden neurons
 
     for (let j = 0; j < net.hiddenLayer.matrix.length; j++) {
       a += 60;
       yOffset += 60;
-      //draw line
-      for (let x = 20; x < 400; x += .1) {
+      //draw lines for the input to hidden nodes
+      for (let x = 20; x < 400; x++) {
         //change width depending on weight strength
-        strokeWeight(Math.pow(abs(net.weightsHidIn.matrix[j][i]), 2) * 4)
+        strokeWeight(Math.pow(abs(net.weightsHidIn.matrix[j][i]), 1.5) * 4);
         //change color depending on positive or negative
         if (net.weightsHidIn.matrix[j][i] > 0) {
           stroke(0, 100, 200)
@@ -50,13 +49,35 @@ function draw() {
       }
     }
   }
+  //draw lines for the hidden to output nodes
+  xOffset = 500;
+  for (let i = 0; i < net.hiddenLayer.matrix.length; i++) {
+    a = -60 + i * -60;
+    yOffset = -10;
+    for (let j = 0; j < net.outLayer.matrix.length; j++) {
+      a += 60;
+      yOffset += 60;
+      for (let x = 20; x < 350; x++) {
+        strokeWeight(Math.pow(abs(net.weightsOutHid.matrix[j][i]), 1.5) * 4);
+        //change color depending on positive or negative
+        if (net.weightsOutHid.matrix[j][i] > 0) {
+          stroke(0, 100, 200);
+        } else {
+          stroke(255, 160, 0)
+        }
+        line(x + xOffset, (-1 * a * b * Math.pow(b, (Math.pow(x - b, 2) / (2 * (Math.pow(c, 2)))))) + yOffset, x + xOffset, (-1 * a * b * Math.pow(b, (Math.pow(x - b, 2) / (2 * (Math.pow(c, 2)))))) + yOffset);
+      }
+    }
+  }
+
   pop();
+  //draw hidden layer neurons
   for (let i = 0; i < net.hiddenLayer.matrix.length; i++) {
     ellipse(500, 50 + i * 60, 40);
   }
+  //draw output layer neurons
   for (let i = 0; i < net.outLayer.matrix.length; i++) {
     ellipse(850, 50 + i * 60, 40);
   }
-
 
 }
--- network.js
+++ network.js
@@ -5,9 +5,13 @@ class Network {
     this.hiddenLayer = new Matrix(hiddenLayer, 1);
     this.outLayer = new Matrix(outLayer, 1);
     this.weightsHidIn = new Matrix(hiddenLayer, inputLayer);
-    this.weightsHidIn.randomize(-1, 1); //randomize the weights number between -10 and 10
+    this.weightsHidIn.randomize(-1, 1); //randomize the weights number between -1 and 1
     this.weightsHidInBias = new Matrix(hiddenLayer, 1);
     this.weightsHidInBias.randomize(1, 1); //initialize the weight biases as 1
+    this.weightsOutHid = new Matrix(outLayer, hiddenLayer);
+    this.weightsOutHid.randomize(-1, 1); //randomize the weights number between -1 and 1
+    this.weightsOutHidBias = new Matrix(outLayer, 1);
+    this.weightsOutHidBias.randomize(1, 1) //initialize the weights biases as 1
   }
   //function to do the feed forward pass of the NN
   feedForward() {
@@ -16,13 +20,26 @@ class Network {
     //calculate nodes in the hidden layer
     this.hiddenLayer.randomize(0, 0); //set every hidden layer node to 0
     for (let i = 0; i < this.hiddenLayer.matrix.length; i++) {
+      //add the  biases
       this.hiddenLayer.matrix[i][0] += this.weightsHidInBias.matrix[i][0];
-      //iterate through each node in the hidden layer
+      //iterate through each node in the input layer
       for (let j = 0; j < this.inputLayer.matrix.length; j++) {
         //iterate through each input
         //do the weighted sum
         this.hiddenLayer.matrix[i][0] += this.weightsHidIn.matrix[i][j] * this.inputLayer.matrix[j][0];
 
+      }
+    }
+    //calculate nodes in the output outLayer
+    this.outLayer.randomize(0, 0) //set every output layer node to 0
+    for (let i = 0; i < this.outLayer.matrix.length; i++) {
+      //add the biases
+      this.outLayer.matrix[i][0] += this.weightsOutHidBias.matrix[i][0];
+      //iterate through each node in the hidden outLayer
+      for (let j = 0; j < this.hiddenLayer.matrix.length; j++) {
+        //iterate through each hidden node
+        //do the weighted sum
+        this.outLayer.matrix[i][0] += this.weightsOutHid.matrix[i][j] * this.hiddenLayer.matrix[j][0];
       }
     }
 
